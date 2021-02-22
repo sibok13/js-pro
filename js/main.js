9d9f4ff14e2fedb9260555a16d92cb01b09fa5c6
sibok <maelstrom@mail.ru>
2021-02-22 11:39:48 +0400

    ver01

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 20000},
+    {id: 2, title: 'Mouse', price: 1500},
+    {id: 3, title: 'Keyboard', price: 5000},
+    {id: 4, title: 'Gamepad', price: 4500},
+    {id: 5},
+];
+
+const renderProduct = (title = "н/д", price = "0") => {
+    return `<div class="product-item">
+                <h3>${title}</h3>
+                <img src="">
+                <p>Цена: ${price} руб.</p>
+                <button class="by-btn">Добавить в корзину</button>
+            </div>`;
+}
+
+const renderProducts = (list) => {
+    const productList = list.forEach((item) => {
+        return document.querySelector('.products').insertAdjacentHTML("beforeEnd", renderProduct(item.title, item.price));
+    });
+}
+
+renderProducts(products);
