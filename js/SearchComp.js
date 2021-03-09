6cf9757ea2aa73063ef5e56943750b2b69cec79e
Dmitriy <maelstrom@mail.ru>
2021-03-09 20:17:47 +0400

    ver02

--- js/SearchComp.js
+++ js/SearchComp.js
@@ -5,13 +5,16 @@ Vue.component('search', {
         }
     },
     methods: {
+        log(){
+            console.log(this.userSearch);
+        },
         filter(){
             let regexp = new RegExp(this.userSearch, 'i');
             this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
     template: `
-    <form action="#" class="search-form" @submit.prevent="filter">
+    <form action="#" class="search-form" @submit.prevent="log">
         <input type="text" class="search-field" v-model="userSearch">
         <button class="btn-search" type="submit">
             <i class="fas fa-search"></i>
